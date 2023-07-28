267943a2f516e8111adcb16b288c057c654700c1
Rodrigo <lucas.herculano@aluno.senai.br>
2023-07-28 11:06:17 -0300

    Entendendo os Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -17,6 +17,11 @@ public class Fundamentos {
         double media = 8.5;
         
         String bairro = new String("Mario Dedini");
+        
+        //Declaração de Wrappers
+        int nota1 = Integer.parseInt("8");
+        double mediaF = Double.parseDouble("6.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
