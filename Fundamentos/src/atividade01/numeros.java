9a1aec9b9f772bf26fdd777cd53a396f7ce2387c
Lucas <lucas.herculano@aluno.senai.com>
2023-08-03 13:34:17 -0300

    Enviando as atividades

--- Fundamentos/src/atividade01/numeros.java
+++ Fundamentos/src/atividade01/numeros.java
@@ -1,5 +1,12 @@
 package atividade01;
 
 public class numeros {
-    
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + "");
+            }
+        }
+    }
 }
